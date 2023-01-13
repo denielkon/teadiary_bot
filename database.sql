ee83af1666865e065123efff15600a5d77a868e3
Данил Охонько <64771499+denielkon@users.noreply.github.com>
2023-01-13 12:23:02 +0300

    refactor: change structure of the project
    add new file
    refactor features
    delete one table in db

--- database.sql
+++ database.sql
@@ -7,18 +7,11 @@ create TABLE stateReview(
    rating INTEGER,
    teaDescription VARCHAR(255)
 );
-create TABLE stateRating(
-   id SERIAL PRIMARY KEY,
-   teaName VARCHAR(255),
-   rating INTEGER,
-   reviewTimes INTEGER
-);
 create TABLE users(
    id SERIAL PRIMARY KEY,
-   teaName VARCHAR(255),
    userID INTEGER,
    newTea INTEGER,
-   FOREIGN KEY (newTea) REFERENCES newTea (id)
+   FOREIGN KEY (newtea) REFERENCES newTea (id)
 );
 create TABLE newTea(
    id SERIAL PRIMARY KEY,
